a23bb7698ef0adefe76eea7d86c7511ed0ed1f17
phimrisa <phimrisa@gmail.com>
2026-01-13 21:11:35 +0700

    Update Product.java

--- Product.java
+++ Product.java
@@ -1,23 +1,23 @@
 public class Product {
-    //Attribute
     public String name;
     public double price = 0;
     public int quantity = 0;
-
-    //Methods
+    
     public void addStock(int amount){
-         quantity += amount;
         if(amount > 0){
+            quantity += amount;
             System.out.println("Added " + amount + " items to stock");
         }
     }
 
     public void sell(int amount){
-        amount += quantity;
-        if(amount <= quantity){
+        if(amount < quantity){
+            quantity -= amount;
             System.out.println("Sold " + amount + " items");
         }
-        System.out.println("Not enough items on stock");
+        else{
+            System.out.println("Not enough items on stock...");
+        }
     }
 
     public void changePrice(double newPrice){
@@ -32,9 +32,9 @@ public class Product {
     }
 
     public void showInfo(){
-        System.out.println("Name       : " + name);
-        System.out.println("Price      : " + price + " Baht/Item");
-        System.out.println("Quantity   : " + quantity + " Items");
+        System.out.println("Name  : " + name);
+        System.out.println("Price : " + price + " Baht/Item");
+        System.out.println("Stock : " + quantity + " Items");
         System.out.println("Stock Value: " + stockValue() + " Baht");
     }
 }
